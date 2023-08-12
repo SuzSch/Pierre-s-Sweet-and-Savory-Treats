fe63f75118e45f27f62cbb2f1f5d0c43123152a0
Suzanne Schuber <suzanneschuber@gmail.com>
2023-08-12 09:54:56 -0700

    Update models and Controllers.

---
+++ TreatShop/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TreatShop.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace TreatShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<TreatShopContext>(
+                              dbContextOptions => dbContextOptions
+                                .UseMySql(
+                                  builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                                )
+                              )
+                            );
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                    .AddEntityFrameworkStores<TreatShopContext>()
+                    .AddDefaultTokenProviders();
+
+            WebApplication app = builder.Build();
+
+            // app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}"
+              );
+
+            app.Run();
+        }
+    }
+}
