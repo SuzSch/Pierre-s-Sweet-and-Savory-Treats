3b1ebe8ba4fe3be472b064e27ffb2a705f057ba9
Suzanne Schuber <suzanneschuber@gmail.com>
2023-08-12 10:52:39 -0700

    Update Accoutns Controller and View Models.

---
+++ TreatShop/Controllers/AccountsController.cs
@@ -0,0 +1,95 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using TreatShop.Models;
+using System.Threading.Tasks;
+using TreatShop.ViewModels;
+
+namespace TreatShop.Controllers
+{
+  public class AccountsController : Controller
+  {
+    private readonly TreatShopContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, TreatShopContext db)
+    {
+      _userManager = userManager;
+      _signInManager = signInManager;
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Register()
+    {
+      ViewBag.Title = "Register User";
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Register(RegisterViewModel model)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(model);
+      }
+      else
+      {
+        ApplicationUser user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
+        IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+        if (result.Succeeded)
+        {
+          Microsoft.AspNetCore.Identity.SignInResult signResult = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, isPersistent: true, lockoutOnFailure: false);
+          if (signResult.Succeeded)
+          {
+            return RedirectToAction("Index");
+          }
+          else
+          {
+            return RedirectToAction("Login");
+          }
+        }
+        else
+        {
+          foreach (IdentityError error in result.Errors)
+          {
+            ModelState.AddModelError("", error.Description);
+          }
+          return View(model);
+        }
+      }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Login(LoginViewModel model)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(model);
+      }
+      else
+      {
+        Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, isPersistent: true, lockoutOnFailure: false);
+        if (result.Succeeded)
+        {
+          return RedirectToAction("Index");
+        }
+        else
+        {
+          ModelState.AddModelError("", "There is something wrong with your email or username. Please try again.");
+          return View(model);
+        }
+      }
+    }
+    [HttpPost]
+    public async Task<ActionResult> LogOff()
+    {
+      await _signInManager.SignOutAsync();
+      return RedirectToAction("Index");
+    }
+
+  }
+}
--- TreatShop/Controllers/HomeController.cs
+++ TreatShop/Controllers/HomeController.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TreatShop.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+
+namespace TreatShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
