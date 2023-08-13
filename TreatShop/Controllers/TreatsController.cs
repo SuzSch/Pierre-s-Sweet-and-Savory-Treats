c182891172c52c9aa6bedc3a9382bed4367d551d
Suzanne Schuber <suzanneschuber@gmail.com>
2023-08-13 09:01:10 -0700

    Add create to controllers and views, WIP getting newly created treats to show on splash page.

--- TreatShop/Controllers/TreatsController.cs
+++ TreatShop/Controllers/TreatsController.cs
@@ -31,5 +31,27 @@ namespace TreatShop.Controllers
                           .ToList();
       return View(userTreats);
     }
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Treat treat)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(treat);
+      }
+      else
+      {
+        string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+        treat.User = currentUser;
+        _db.Treats.Add(treat);
+        _db.SaveChanges();
+        return RedirectToAction("Index", "Home");
+      }
+    }
   }
 }
